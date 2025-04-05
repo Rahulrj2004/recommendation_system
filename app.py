918f7a66254d1c81dccb7d20cf2b3b8119081179
rahulrj2004 <r2004jawalkar@gmail.com>
2025-04-05 20:05:21 +0530

    display 50 popular books

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 import pickle
 
 app = Flask(__name__)
@@ -10,16 +10,28 @@ app = Flask(__name__)
 # Load the dataset (popular_df)
 popular_df = pickle.load(open("popular.pkl","rb"))
 
-
+book_name = list(popular_df["Book-Title"].values)
+author = list(popular_df["Book-Author"].values)
+image = list(popular_df["Image-URL-M"].values)
+votes = list(popular_df["num_ratings"].values)
+rating = list(popular_df["avg_ratings"].values)
 
 @app.route("/")
 def index():
-    return render_template("index.html",
-                           book_name = list(popular_df["Book-Title"].values),
-                           author = list(popular_df["Book-Author"].values),
-                           image = list(popular_df["Image-URL-M"].values),
-                           votes = list(popular_df["num_ratings"].values),
-                           rating = list(popular_df["avg_ratings"].values),)
+    return render_template("index.html")
+
+@app.route("/book-all")
+def get_books():
+    books = []
+    for i in range(len(book_name)):
+        books.append({
+            "name": str(book_name[i]),
+            "author":str(author[i]), 
+            "image":str(image[i]),
+            "votes":int(votes[i]),
+            "rating":round(float(rating[i]),2)
+        })
+    return jsonify(books) 
 
 # @app.route("/recommend", methods=["POST"])
 # def recommend():
