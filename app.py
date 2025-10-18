8766f89ed3763230de9b8f3ef920f82cfb7c7a01
rahulrj2004 <r2004jawalkar@gmail.com>
2025-10-18 19:45:42 +0530

    making it render ready

--- app.py
+++ app.py
@@ -266,9 +266,9 @@ def Srecommend():
         })
     return jsonify(recommend_songs)
 
-import os
 
 if __name__ == "__main__":
     # Use PORT from Render, default to 5000 locally
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host="0.0.0.0", port=port)
+    app.run()
